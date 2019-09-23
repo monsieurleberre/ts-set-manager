8df4a69b3b79d6673b0bf483ba19c4036d9db710
monsieurleberre <monsieurleberre@users.noreply.github.com>
2019-09-23 04:45:30 +0100

    To be ignored.
    Will find a better way to do that later

---
+++ src/Apis/ApiKeys.ts
@@ -0,0 +1,3 @@
+export const fortmaticApiKey = 'get your own at https://fortmatic.com/'
+export const cryptoCompareApiKey = 'get your own at https://min-api.cryptocompare.com/pricing'
+export const coinMarketCapApiKey = 'get your own at https://pro.coinmarketcap.com/signup/?plan=0'
